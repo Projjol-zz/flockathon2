475392b28ddb067276bfa2d812363c1c310f5e72
Projjol <probaner23@gmail.com>
2017-02-19 10:18:18 +0530

    threaded

--- server.py
+++ server.py
@@ -167,4 +167,4 @@ def user_count():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     # app.run(host='0.0.0.0', port=port)
-    app.run(host='0.0.0.0', port=port,debug=True)
+    app.run(host='0.0.0.0', port=port,debug=True, threaded=True)
